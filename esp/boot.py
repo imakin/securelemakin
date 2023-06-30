4f1fa2408c148819ee962e44d223b9b365d90d09
izzulmakin <makin.ugm@gmail.com>
2023-06-30 11:16:11 +0700

    add on boot default method/api

--- esp/boot.py
+++ esp/boot.py
@@ -8,6 +8,10 @@ expected_lib = ['base64.mpy', 'binascii.mpy', 'hmac.mpy', 'struct.mpy']
 if os.listdir('/lib')!=expected_lib:
     import libinstall
 
+#my basic API
+def readfile(filename):
+    with open(filename) as f:
+        return f.read()
 
 #main app
 import atmega32u4
